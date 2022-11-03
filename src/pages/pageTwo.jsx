e116702b5f3686249c26eab261e3645f23995540
niteshn1 <niteshnegi50@gmail.com>
2022-11-03 13:00:06 +0530

    delete a tag

---
+++ src/pages/pageTwo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const PageTwo = () => {
+    var count=10
+
+    return (
+        <h1>
+            Page Two size {count}
+        </h1>
+
+    )
+}
+
+export default PageTwo
